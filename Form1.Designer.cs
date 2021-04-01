54b114e0c5a626d99f52681d2f2a544bf1cbaa48
Saša Barišić <sasa.barisic@serengetitech.com>
2021-04-01 15:51:40 +0200

    Added project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TurboMapCalculator {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.propGrid = new System.Windows.Forms.PropertyGrid();
+			this.btnRecalc = new System.Windows.Forms.Button();
+			this.btnPasteImage = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(287, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(574, 507);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// propGrid
+			// 
+			this.propGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.propGrid.Location = new System.Drawing.Point(12, 105);
+			this.propGrid.Name = "propGrid";
+			this.propGrid.Size = new System.Drawing.Size(269, 563);
+			this.propGrid.TabIndex = 1;
+			// 
+			// btnRecalc
+			// 
+			this.btnRecalc.Location = new System.Drawing.Point(12, 12);
+			this.btnRecalc.Name = "btnRecalc";
+			this.btnRecalc.Size = new System.Drawing.Size(269, 23);
+			this.btnRecalc.TabIndex = 2;
+			this.btnRecalc.Text = "Recalculate";
+			this.btnRecalc.UseVisualStyleBackColor = true;
+			this.btnRecalc.Click += new System.EventHandler(this.btnRecalc_Click);
+			// 
+			// btnPasteImage
+			// 
+			this.btnPasteImage.Location = new System.Drawing.Point(12, 41);
+			this.btnPasteImage.Name = "btnPasteImage";
+			this.btnPasteImage.Size = new System.Drawing.Size(269, 23);
+			this.btnPasteImage.TabIndex = 3;
+			this.btnPasteImage.Text = "Paste Image";
+			this.btnPasteImage.UseVisualStyleBackColor = true;
+			this.btnPasteImage.Click += new System.EventHandler(this.btnPasteImage_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(971, 680);
+			this.Controls.Add(this.btnPasteImage);
+			this.Controls.Add(this.btnRecalc);
+			this.Controls.Add(this.propGrid);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PropertyGrid propGrid;
+		private System.Windows.Forms.Button btnRecalc;
+		private System.Windows.Forms.Button btnPasteImage;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace TurboMapCalculator {
+	public partial class Form1 : Form {
+		public EditProps Props = new EditProps();
+
+		PictureBox Overlay;
+		Bitmap OverlayBmp;
+
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e) {
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			propGrid.SelectedObject = Props;
+			propGrid.PropertyValueChanged += PropGrid_PropertyValueChanged;
+
+			DragDrop += Form1_DragDrop;
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e) {
+
+		}
+
+		private void PropGrid_PropertyValueChanged(object s, PropertyValueChangedEventArgs e) {
+		}
+
+		private void btnRecalc_Click(object sender, EventArgs e) {
+			Draw((G) => {
+				G.Clear(Color.Transparent);
+
+				G.FillRectangle(Brushes.Red, new RectangleF(Props.BottomLeft.X, Props.BottomLeft.Y, 10, 10));
+			});
+		}
+
+		private void btnPasteImage_Click(object sender, EventArgs e) {
+			if (!Clipboard.ContainsImage())
+				return;
+
+			Image Img = Clipboard.GetImage();
+			pictureBox1.Image = Img;
+			pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+			OverlayBmp = new Bitmap(Img.Width, Img.Height);
+
+			if (Overlay == null) {
+				Overlay = new PictureBox();
+				Controls.Add(Overlay);
+				Overlay.Parent = pictureBox1;
+			}
+
+			Overlay.Size = pictureBox1.Size;
+			Overlay.BringToFront();
+
+			Overlay.BackColor = Color.Transparent;
+		}
+
+		void Draw(Action<Graphics> A) {
+			using (Graphics Gfx = Graphics.FromImage(OverlayBmp)) {
+				A(Gfx);
+				Gfx.Flush();
+			}
+
+			Overlay.Image = OverlayBmp;
+		}
+	}
+
+	public class EditProps {
+		[Category("Compressor Map")]
+		public Point BottomLeft {
+			get; set;
+		}
+
+		[Category("Engine")]
+		[DisplayName("Displacement")]
+		[Description("Displacement [L]")]
+		public float Displacement {
+			get; set;
+		} = 1.4f;
+	}
+}
